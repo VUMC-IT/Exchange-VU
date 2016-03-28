cf118301b5be7e306449669f374a992d805c71e5
JeremyPratt <jeremy.pratt@vanderbilt.edu>
2016-03-28 10:33:44 -0500

    Update Remove_Quotas.ps1

--- MBX_Quota/Remove_Quotas.ps1
+++ MBX_Quota/Remove_Quotas.ps1
@@ -3,5 +3,6 @@ $Users = @(Get-Content -Path  .\Remove_quota.csv)
 
 foreach($User in $Users)
 {
+Write-Host "Working on ... $User"
 Set-Mailbox -identity $User.Trim() -UseDatabaseQuotaDefaults $false -IssueWarningQuota unlimited -ProhibitSendQuota unlimited -ProhibitSendReceiveQuota unlimited -Confirm:$false 
 }
