eab457b65330c27b098a13b3abfc80c278bb4ca1
Thomas Chandler <thomas.chandler@vanderbilt.edu>
2016-06-03 15:20:11 -0500

    block and unblock
    Removed the whatifs and created the opposing unblock script

--- blockspammer/unblockspammer.ps1
+++ blockspammer/unblockspammer.ps1
@@ -5,7 +5,7 @@
 	 Created on:   	5/11/2016 3:10 PM
 	 Created by:   	chanct 
 	 Organization: 	 
-	 Filename: blockspammer.ps1     	
+	 Filename: unblockspammer.ps1     	
 	===========================================================================
 	.DESCRIPTION
 		A description of the file.
@@ -18,14 +18,13 @@ catch
 	Import-PSSession -Session $exsess
 }
 
-$user = Read-Host -Prompt "Type Vunet of user to block"
+$user = Read-Host -Prompt "Type Vunet of user to unblock"
 
-add-adgroupmember -identity "vmail_internalblocked" -members $user 
-Write-Host ("Adding " + $user + " to internal block list`r`n") -ForegroundColor Yellow
-add-adgroupmember -identity "vmail_externalblocked" -members $user 
-Write-Host ("Adding " + $user + " to external block list`r`n") -ForegroundColor Yellow
-New-MailboxRepairRequest -Mailbox $user -corruptiontype ProvisionedFolder, SearchFolder, AggregateCounts, Folderview 
-Write-Host ("Starting mailbox repair on " + $user + ". This will sever all existing client connections`r`n") -ForegroundColor Yellow
+remove-adgroupmember -identity "vmail_internalblocked" -members $user 
+Write-Host ("Removing " + $user + " from internal block list`r`n") -ForegroundColor Yellow
+remove-adgroupmember -identity "vmail_externalblocked" -members $user 
+Write-Host ("Removing " + $user + " from external block list`r`n") -ForegroundColor Yellow
+
 Write-Host ("The newly spawned window will contain the inbox rules for " + $user + ". Please examine for suspicious rules - IE Send all mail to Deleted Items. 
     Ctrl-click any rules to be removed and press OK`r`n") -ForegroundColor Yellow
 $smtp = Get-mailbox $user | select primarysmtpaddress
@@ -33,5 +32,5 @@ $smtp = $smtp.primarysmtpaddress
 $badrules = get-inboxrule -mailbox $smtp | select ruleidentity, description | Out-GridView -passthru
 $badrules.RuleIdentity | remove-inboxrule -mailbox $user 
 Write-Host ("Removed malicious rules from " + $user + "`r`n") -ForegroundColor Yellow
-Write-Host ("Remember to log the block in sharepoint: 
+Write-Host ("Remember to log the unblock in sharepoint: 
     https://vanderbilt365.sharepoint.com/sites/VUIT/Hosting_services/Collaboration/_layouts/15/start.aspx#/Lists/BlockedEmailTracking/Active%20Email%20Blocks.aspx") -ForegroundColor Yellow
